c36de9a922907d7bcd2798ed3b64b90e4602046b
Erik Golinelli <erik@codekraft.it>
2024-06-06 17:44:26 +0200

    the edit component

--- src/menu/Edit.tsx
+++ src/menu/Edit.tsx
@@ -1,116 +1,66 @@
 /**
  * External dependencies
  */
-import { compose } from '@wordpress/compose';
 /**
  * WordPress dependencies
  */
-import { useRef, useState } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import classnames from 'classnames';
-import { Controls } from './controls';
-import { InnerBlocks, useInnerBlocksProps } from '@wordpress/block-editor';
+import { Controls } from './Controls';
+import { InnerBlocks } from '@wordpress/block-editor';
 import { Button } from '@wordpress/components';
-import type { Template } from '@wordpress/blocks';
-import { withSelect } from '@wordpress/data';
+import type { BlockAttributes } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
+import { MegaMenuAttributes, TEMPLATE } from './constants';
+import { withInstanceId } from '@wordpress/compose';
+import useInstanceId from '@wordpress/compose/src/hooks/use-instance-id';
 
-const TEMPLATE = [
-	[
-		'core/group',
-		{
-			layout: {
-				type: 'flex',
-				flexWrap: 'nowrap',
-				verticalAlignment: 'stretch',
-			},
-		},
-		[ [ 'megamenu/menu-item', {} ] ],
-	],
-] as Template[];
-
-const ALLOWED_BLOCKS = [
-	'megamenu/menu-item',
-	'codekraft/oh-my-svg',
-	'core/group',
-	'core/site-logo',
-	'core/social-link',
-];
-
-function MegaMenu( args ) {
+/**
+ * The `MegaMenu` edit component.
+ * @param args
+ * @param args.selectedBlockHasDescendants
+ * @param args.isImmediateParentOfSelectedBlock
+ * @param args.isSelected
+ * @param args.attributes
+ * @param args.setAttributes
+ * @param args.clientId
+ */
+export default function Edit( args: {
+	isSelected: boolean;
+	attributes: MegaMenuAttributes;
+	setAttributes: ( attributes: BlockAttributes ) => void;
+	clientId: string;
+} ): JSX.Element {
 	const {
-		selectedBlockHasDescendants,
-		isImmediateParentOfSelectedBlock,
-		isSelected,
-		attributes,
+		clientId,
+		attributes: {
+			activator,
+			expandDropdown,
+			collapseOnMobile,
+			responsiveBreakpoint,
+			dropdownMaxWidth,
+			menuAlign,
+		},
+		isSelected: boolean,
+		setAttributes,
 	} = args;
 
-	const [ showResponsiveMenu, setShowResponsiveMenu ] = useState( false );
-	const ref = useRef( null );
-
-	return (
-		<div>
-			<Controls
-				{ ...args }
-				showResponsiveMenu={ showResponsiveMenu }
-				setShowResponsiveMenu={ setShowResponsiveMenu }
-			/>
-			<nav
-				className={ classnames(
-					'wp-block-megamenu',
-					`activator-${ attributes.activator }`,
-					{
-						'is-hidden': showResponsiveMenu,
-						[ `has-full-width-dropdown` ]:
-							attributes.expandDropdown ||
-							attributes.dropdownMaxWidth === 0,
-						[ `is-collapsible` ]: attributes.collapseOnMobile,
-					}
-				) }
-				data-responsive-breakpoint={ attributes.responsiveBreakpoint }
-				data-dropdown-content-width={ attributes.dropdownMaxWidth }
-			>
-				<div className={ 'wp-block-megamenu__content' }>
-					<InnerBlocks
-						ref={ ref }
-						template={ TEMPLATE }
-						templateLock={ false }
-						allowedBlocks={ ALLOWED_BLOCKS }
-						templateInsertUpdatesSelection={ false }
-						renderAppender={
-							( isImmediateParentOfSelectedBlock &&
-								! selectedBlockHasDescendants ) ||
-							isSelected
-								? InnerBlocks.DefaultBlockAppender
-								: undefined
-						}
-						orientation="horizontal"
-					/>
-				</div>
-			</nav>
-			<div
-				className={ classnames(
-					'wp-block-megamenu__toggle-wrapper',
-					`align-${ attributes.menuAlign || 'right' }`,
-					{
-						'is-hidden': ! showResponsiveMenu,
-					}
-				) }
-			>
-				<Button className="wp-block-megamenu__toggle hamburger">
-					<div></div>
-				</Button>
-			</div>
-		</div>
-	);
-}
-
-export default compose( [
-	withSelect( ( select, { clientId } ) => {
+	useSelect( ( select, {} ) => {
 		const {
 			getClientIdsOfDescendants,
 			hasSelectedInnerBlock,
 			getSelectedBlockClientId,
 			getBlocksByClientId,
+		}: {
+			withInstanceId: string;
+			getClientIdsOfDescendants: Function;
+			hasSelectedInnerBlock: Function;
+			getSelectedBlockClientId: Function;
+			getBlocksByClientId: Function;
+			getBlock: Function;
 		} = select( 'core/block-editor' );
+		const clientId = args.clientId;
+		// Returns true if one of the block’s inner blocks is selected.
 		const isImmediateParentOfSelectedBlock = hasSelectedInnerBlock(
 			clientId,
 			false
@@ -126,5 +76,54 @@ export default compose( [
 			selectedBlockHasDescendants,
 			menuItems,
 		};
-	} ),
-] as any )( MegaMenu );
+	}, [] );
+
+	const [ showResponsiveMenu, setShowResponsiveMenu ] = useState( false );
+
+	return (
+		<div>
+			<Controls
+				showResponsiveMenu={ showResponsiveMenu }
+				setShowResponsiveMenu={ setShowResponsiveMenu }
+				attributes={ args.attributes }
+				setAttributes={ setAttributes }
+			/>
+			<nav
+				className={ classnames(
+					'wp-block-megamenu',
+					`activator-${ activator }`,
+					{
+						'is-hidden': showResponsiveMenu,
+						[ `has-full-width-dropdown` ]:
+							expandDropdown || dropdownMaxWidth === 0,
+						[ `is-collapsible` ]: collapseOnMobile,
+					}
+				) }
+				data-responsive-breakpoint={ responsiveBreakpoint }
+				data-dropdown-content-width={ dropdownMaxWidth }
+			>
+				<div className={ 'wp-block-megamenu__content' }>
+					<InnerBlocks
+						orientation="horizontal"
+						templateLock={ false }
+						template={ TEMPLATE }
+						templateInsertUpdatesSelection={ false }
+					/>
+				</div>
+			</nav>
+			<div
+				className={ classnames(
+					'wp-block-megamenu__toggle-wrapper',
+					`align-${ menuAlign || 'right' }`,
+					{
+						'is-hidden': ! showResponsiveMenu,
+					}
+				) }
+			>
+				<Button className="wp-block-megamenu__toggle hamburger">
+					<div></div>
+				</Button>
+			</div>
+		</div>
+	);
+}
