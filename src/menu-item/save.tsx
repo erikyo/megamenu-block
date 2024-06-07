ac50951afcc0228a94ad062280b872ae0c4c341f
Erik Golinelli <erik@codekraft.it>
2024-06-07 11:42:54 +0200

    menu item save function

--- src/menu-item/save.tsx
+++ src/menu-item/save.tsx
@@ -1,19 +1,29 @@
 /**
  * WordPress dependencies
  */
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import {
+	InnerBlocks,
+	useBlockProps,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
 import { Icon } from '@wordpress/components';
 import { chevronDown } from '@wordpress/icons';
 import classnames from 'classnames';
+import { type BlockAttributes, type BlockEditProps } from '@wordpress/blocks';
+import { MenuItemAttributes } from './constants';
 
-export default function save( { attributes } ) {
-	const {
-		url,
-		linkTarget,
-		rel,
-		text,
-		parentAttributes: { menuItemHasChildrens, menusMinWidth, align },
-	} = attributes;
+export default function save( {
+	attributes,
+}: BlockEditProps< BlockAttributes > ) {
+	const { url, linkTarget, rel, text, parentAttributes } =
+		attributes as MenuItemAttributes;
+	const { hasDescendants, menusMinWidth, align } = parentAttributes;
+
+	const blockProps = useBlockProps.save( {
+		className: classnames( 'wp-block-megamenu-item', {
+			'has-children': hasDescendants || false,
+		} ),
+	} );
 
 	const linkProps = {
 		href: url ? url : '#',
@@ -21,43 +31,37 @@ export default function save( { attributes } ) {
 		...( rel && { rel } ),
 	};
 
-	const blockProps = useBlockProps.save( {
-		className: classnames( 'wp-block-megamenu-item', {
-			'has-children': menuItemHasChildrens || false,
-		} ),
-	} );
-
 	return (
-		<>
-			<div { ...blockProps }>
-				<a
-					{ ...linkProps }
-					className={ 'wp-block-megamenu-item__link' }
-					style={ {
-						minWidth: menusMinWidth ? menusMinWidth + 'px' : 'auto',
-						justifyContent: align ? align : 'left',
-					} }
-				>
-					<span className={ 'wp-block-megamenu-item__text' }>
-						{ text }
-					</span>
-					{ menuItemHasChildrens && (
-						<Icon
-							style={ {
-								fill: 'currentColor',
-							} }
-							icon={ chevronDown }
-							className="wp-block-megamenu-item__toggle"
-							aria-hidden="true"
-						/>
-					) }
-				</a>
-				{ menuItemHasChildrens && (
-					<div className={ 'wp-block-megamenu-item__dropdown' }>
-						<InnerBlocks.Content />
-					</div>
+		<div { ...blockProps }>
+			<a
+				{ ...linkProps }
+				className={ 'wp-block-megamenu-item__link' }
+				style={ {
+					minWidth: menusMinWidth ? menusMinWidth + 'px' : 'auto',
+					justifyContent: align ? align : 'left',
+				} }
+			>
+				<span className={ 'wp-block-megamenu-item__text' }>
+					{ text }
+				</span>
+				{ hasDescendants && (
+					<Icon
+						style={ {
+							fill: 'currentColor',
+						} }
+						icon={ chevronDown }
+						className="wp-block-megamenu-item__toggle"
+						aria-hidden="true"
+					/>
 				) }
-			</div>
-		</>
+			</a>
+			{ hasDescendants && (
+				<div
+					{ ...useInnerBlocksProps.save( {
+						className: 'wp-block-megamenu-item__dropdown',
+					} ) }
+				/>
+			) }
+		</div>
 	);
 }
