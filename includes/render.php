865a576ebee58d25e54232192b70f26aed9a437c
Erik Golinelli <erik@codekraft.it>
2026-05-23 14:30:34 +0200

    Refactor `render.php` to simplify megamenu attributes and add interactive data bindings.

--- includes/render.php
+++ includes/render.php
@@ -21,8 +21,6 @@ function render_megamenu_menu( $attributes, $content, $block ) {
 	$collapse_on_mobile   = $attributes['collapseOnMobile'] ?? true;
 	$responsive_breakpoint = $attributes['responsiveBreakpoint'] ?? 1023;
 	$dropdown_max_width   = $attributes['dropdownMaxWidth'] ?? 0;
-	$menu_align           = $attributes['menuAlign'] ?? 'right';
-	$hamburger_color      = $attributes['hamburgerColor'] ?? 'var(--wp--preset--color--black)';
 
 	$classes = array(
 		'wp-block-megamenu',
@@ -44,17 +42,28 @@ function render_megamenu_menu( $attributes, $content, $block ) {
 		'data-responsive-breakpoint' => esc_attr( $responsive_breakpoint ),
 		'data-dropdown-width' => esc_attr( $dropdown_max_width ),
 		'data-activator' => esc_attr( $activator ),
+		'data-wp-interactive' => 'megamenu',
+		'data-wp-context' => wp_json_encode( array(
+			'breakpoint' => $responsive_breakpoint,
+			'activator' => $activator,
+		) ),
+		'data-wp-init' => 'callbacks.initResponsive',
 	) );
 
 	ob_start();
 	?>
 	<nav <?php echo $wrapper_attrs; ?>>
-		<div class="wp-block-megamenu__content">
+		<div class="wp-block-megamenu__content" data-wp-bind--class="{'is-opened': state.isMobileMenuOpen}">
 			<?php echo $content; ?>
 		</div>
 	</nav>
-	<div class="wp-block-megamenu__toggle-wrapper align<?php echo esc_attr( $menu_align ); ?>">
-		<button class="wp-block-megamenu__toggle hamburger" aria-label="Toggle megamenu" style="color: <?php echo esc_attr( $hamburger_color ); ?>;">
+	<div class="wp-block-megamenu__toggle-wrapper">
+		<button 
+			class="wp-block-megamenu__toggle hamburger" 
+			aria-label="Toggle megamenu"
+			data-wp-on--click="actions.toggleMobileMenu"
+			data-wp-bind--aria-expanded="state.isMobileMenuOpen"
+		>
 			<div></div>
 		</button>
 	</div>
@@ -122,8 +131,8 @@ function render_megamenu_menu_item( $attributes, $content, $block ) {
 
 	ob_start();
 	?>
-	<div <?php echo $wrapper_attrs; ?>>
-		<a href="<?php echo $href; ?>"<?php echo $target_attr; ?><?php echo $rel_attr; ?> class="wp-block-megamenu-item__link" style="<?php echo esc_attr( $link_style ); ?>">
+	<div <?php echo $wrapper_attrs; ?> data-wp-interactive="megamenu">
+		<a href="<?php echo $href; ?>"<?php echo $target_attr; ?><?php echo $rel_attr; ?> class="wp-block-megamenu-item__link" style="<?php echo esc_attr( $link_style ); ?>" data-wp-on--click="actions.openDropdown" data-wp-on--mouseleave="actions.closeDropdown">
 			<span class="wp-block-megamenu-item__text"><?php echo wp_kses_post( $text ); ?></span>
 			<?php if ( $has_descendants ) : ?>
 				<span class="wp-block-megamenu-item__toggle" aria-hidden="true" style="fill: currentColor;">
